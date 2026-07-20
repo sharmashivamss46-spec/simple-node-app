f75f5bd464f9b7268cbe35af76b4e1aee76e846b
yash5878 <157448053+yash5878@users.noreply.github.com>
2026-07-20 10:05:23 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello from Jenkins CI/CD Pipeline");
+});
+
+app.listen(3000, () => {
+    console.log("Server started");
+});
